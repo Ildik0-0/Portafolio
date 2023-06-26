85c47a928d159e6c4ba3d1ffd00f38002682381c
Ildik0-0 <iremar.rivas@gmail.com>
2023-06-25 22:48:34 -0400

    first commit

---
+++ client/src/components/SideBar/sideBar.js
@@ -0,0 +1,29 @@
+import { Link, NavLink } from 'react-router-dom'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faHome} from '@fortawesome/free-solid-svg-icons'
+import './sideBar.scss'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+
+
+const SideBar = () => {
+    return (
+       <div className='nav-bar'>
+        <Link className='logo' to='/'>
+            <img src={LogoS} alt='logo'/>
+            <img className='sub-logo' src={LogoSubtitle} alt='slobodan'/>
+
+        </Link>
+
+        <nav>
+            <NavLink exact = 'true' activeclassname='active' to='/'>
+                <FontAwesomeIcon icon={faHome} color='#4d4d4e'/>
+
+            </NavLink>
+        </nav>
+
+       </div>
+    )
+}
+//jjjihio
+export default SideBar
---
+++ client/src/components/layoutPage/index.js
@@ -0,0 +1,14 @@
+//parent for all the componets
+import SideBar from '../SideBar/sideBar';
+import './index.scss'
+
+const Layout = () =>{
+    return (
+        <>
+            <SideBar/>
+
+        </>
+    )
+}
+
+export default Layout;
